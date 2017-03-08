9b939f5c1226c40552a4275a7160041b2374007c
Nana Asiedu-Ampem <na1814@ic.ac.uk>
2017-03-08 18:55:05 +0000

    Intial completion of replica

--- replica.erl
+++ replica.erl
@@ -15,15 +15,19 @@ next(State, Slot_in, Slot_out, Request, Proposals, Decision, Leaders) ->
   receive
     {request, C} ->      % request from client
       Request2 = Request ++ [C],
-      {Decisions2, Slot_in2, Proposals2} = {Decision, Slot_in, Proposals}
+      {Decisions2, Slot_in2, Proposals2} = {Decision, Slot_in, Proposals},
+      Slot_out2 = Slot_out;
 
     {decision, S, C} ->  % decision from commander
       Decisions2 = Decisions ++ [{S, C}],
-      {Slot_in2, Proposals2, Requests2} = decide (Decisions2)
+      {Slot_out2, Proposals2, Requests2} = decide (Decisions2),
+      Slot_in2 = Slot_in
+
   end, % receive
 
-  ... = propose(...),
-  next().
+  {Slot_in3, Proposals3, Requests3} = propose(Slot_in2, Slot_out2, Proposals2,
+                                              Requests2, Leaders, Decisions2),
+  next(State, Slot_in3, Slot_out2, Request3, Proposals3, Decision2, Leaders).
 
 propose(Slot_in, Slot_out, Proposals, Requests, Leaders, Decisions) ->
   WINDOW = 5,
@@ -43,15 +47,52 @@ propose(Slot_in, Slot_out, Proposals, Requests, Leaders, Decisions) ->
   true -> {Slot_in, Proposals, Requests}
   end.
 
-decide(...) ->
-  
-       perform(...),
-  ...
+decide(Decisions, Database, Proposals, Requests, Slot_out) ->
+  C_Slotout_D = [ C | {S, C} <- Decisions, S == Slot_out ],
+  C_Slotout_P = [ {S, C} | {S, C} <- Proposals, S == Slot_out ]
+  decide(Decisions, Database, Proposals, Requests, Slot_out,
+         C_Slotout_D, C_Slotout_P).
 
-perform(...) ->
-  ...
-      Database ! {execute, Op},
-      Client   ! {response, Cid, ok}
+decide(Decisions, Database, Proposals, Requests, Slot_out,
+       [C | Rest], []) ->
+  Slot_out2 = perform(Database, C, Decision, Slot_out),
+  decide(Decisions, Database, Proposals, Requests, Slot_out2,
+         Rest, []).
+
+decide(Decisions, Database, Proposals, Requests, Slot_out, [], _) ->
+  {Slot_out, Proposals, Requests};
+
+decide(Decisions, Database, Proposals, Requests, Slot_out,
+       [C | RestD], [ {S, Cp} | RestP]) ->
+  Proposals2 = Proposals -- [{S, Cp}],
+  Requests2 =
+    if C /= Cp ->
+      Requests ++ [Cp];
+    true ->
+      Requests
+    end,
+  Slot_out2 = perform(Database, C, Decision, Slot_out),
+  decide(Decisions, Database, Proposals2, Requests2, Slot_out2,
+         RestD, RestP).
+
+perform(Database, {Client, Op, Cid}, Decisions, Slot_out) ->
+  Slot_out2 = if has_lower_slot(Slot_out, {Client, Op, Cid}, Decisions) ->
+                Slot_out + 1;
+              true ->
+                Slot_out
+              end,
+  Database ! {execute, Op},
+  Slot_out3 = Slot_out2 + 1,
+  Client ! {response, Cid, ok},
+  Slot_out3.
+
+has_lower_slot(Slot_out, C, [ {S, Cp} | Ds]) ->
+  if
+    S < Slot_out and C == Cp -> true;
+    true                     -> has_lower_slot(Slot_out, C, Ds)
+  end;
+
+has_lower_slot(Slot_out, C, []) -> false.
 
 slot_in_lists(Slot, []) -> false;
 
